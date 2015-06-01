702a7c511741e3117eb66d977fabfb4834c5b904
hiukim <hiukim@gmail.com>
2015-06-01 18:53:36 +0800

    admin access

--- server/slack/publishes.es6.js
+++ server/slack/publishes.es6.js
@@ -1,12 +1,12 @@
 Meteor.publish('slackTeams', function() {
-  if (!this.userId) return [];
+  if (!Users.isAdmin(this.userId)) return [];
   return [
     SlackService.Teams.find()
   ]
 });
 
 Meteor.publish('slackChannels', function() {
-  if (!this.userId) return [];
+  if (!Users.isAdmin(this.userId)) return [];
   return [
     D.Channels.find({category: D.Channels.Categories.SLACK})
   ]
